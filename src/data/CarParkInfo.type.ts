d9c62a794387aaceb85bbc69549b11552eba9795
maxwong <max.venturenix@gmail.com>
2024-12-16 10:52:07 +0800

    Step 2 and Step 3

---
+++ src/data/CarParkInfo.type.ts
@@ -0,0 +1,45 @@
+export interface CarkParkInfoDto {
+  results: Result[];
+}
+
+export interface Result {
+  park_Id: string;
+  name: string;
+  displayAddress: string;
+  district: string;
+  latitude: number;
+  longitude: number;
+  renditionUrls?: RenditionUrls;
+  privateCar?: PrivateCar;
+  LGV?: Lgv;
+  HGV?: Hgv;
+  coach?: Coach;
+  motorCycle?: MotorCycle;
+}
+
+export interface RenditionUrls {
+  square?: string;
+  thumbnail?: string;
+  banner?: string;
+  carpark_photo?: string;
+}
+
+export interface PrivateCar {
+  space: number;
+}
+
+export interface Lgv {
+  space: number;
+}
+
+export interface Hgv {
+  space: number;
+}
+
+export interface Coach {
+  space: number;
+}
+
+export interface MotorCycle {
+  space: number;
+}
